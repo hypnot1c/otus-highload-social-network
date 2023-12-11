cf4c82f81397edde3a4ac74c16aa211485d28de9
hypnot1c <ahmed_@mail.ru>
2023-12-12 00:44:56 +0300

    updat edb init script

--- src/Apps/OTUS.HA.SN.Web.App.Auth/Resources/WebApplicationBuilder/DbContextWebApplicationBuilderConfigurator.cs
+++ src/Apps/OTUS.HA.SN.Web.App.Auth/Resources/WebApplicationBuilder/DbContextWebApplicationBuilderConfigurator.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using OTUS.HS.SN.Data.Auth.Context;
+using OTUS.HS.SN.Data.Master.Context;
 
 namespace OTUS.HA.SN.Web.App.Auth.Resources;
 
@@ -7,6 +8,9 @@ internal class DbContextWebApplicationBuilderConfigurator : IWebApplicationBuild
 {
   public WebApplicationBuilder AddServices(WebApplicationBuilder builder, IConfiguration config)
   {
+    builder.Services.AddDbContext<MasterContext>(options => options
+      .UseNpgsql(builder.Configuration.GetConnectionString("MasterContext"))
+    );
     builder.Services.AddDbContext<AuthContext>(options => options
       .UseNpgsql(builder.Configuration.GetConnectionString("AuthContext"))
     );
